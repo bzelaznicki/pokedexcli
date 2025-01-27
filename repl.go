50268e4c7610ed2d60d582ba39d6fa1ee2a6570e
bzelaznicki <bartosz@zelaznicki.com>
2025-01-27 10:03:17 +0100

    added interactions

--- repl.go
+++ repl.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -10,4 +13,22 @@ func cleanInput(text string) []string {
 	cleanedInput := strings.Fields(lowerCase)
 
 	return cleanedInput
+}
+
+func startRepl() {
+	fmt.Println("Welcome to the Pokédex!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			cleaned := cleanInput(scanner.Text())
+			if len(cleaned) == 0 {
+				continue
+			}
+			fmt.Println("Your command was:", cleaned[0])
+		}
+
+	}
 }
